21f85eaba700e4af55cf9b3f728afec3f2ccba07
Евгений Гаврилин <evgenijgavrilin@MacBook-Pro-Evgenij-2.local>
2023-06-19 02:36:47 +0300

    добавил анализатор кода

---
+++ cmd/mycheck/mycheck.go
@@ -0,0 +1,31 @@
+package osexitcheckanalyzer
+
+import (
+	"go/ast"
+
+	"golang.org/x/tools/go/analysis"
+)
+
+var OsExitCheckAnalyzer = &analysis.Analyzer{
+	Name: "osexitcheck",
+	Doc:  "check for os.Exit usage in main",
+	Run:  run,
+}
+
+func run(pass *analysis.Pass) (interface{}, error) {
+	for _, file := range pass.Files {
+		// функцией ast.Inspect проходим по всем узлам AST
+		ast.Inspect(file, func(n ast.Node) bool {
+			if c, ok := n.(*ast.CallExpr); ok {
+				if s, ok := c.Fun.(*ast.SelectorExpr); ok {
+					// только функции Println
+					if s.Sel.Name == "Exit" {
+						pass.Reportf(s.Pos(), "used Exit")
+					}
+				}
+			}
+			return true
+		})
+	}
+	return nil, nil
+}
